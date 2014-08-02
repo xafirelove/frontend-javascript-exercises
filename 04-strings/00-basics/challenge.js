473a074cf82f9927a2638e69a8494ef3394f472f
xafirelove <xafirelove@outlook.com>
2014-08-02 08:52:59 -0400

    modified challenge file

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -5,24 +5,24 @@ function formLetter(firstName, senderName, message)
         var finalString = stringGreeting.concat(firstName);
         finalString += ",\n\n" + message;
         finalString += "\n\n" + stringClosing + "\n\n" + senderName;
-    }
+    };
 
 function sliceItAndCombineIt(bigString, startA, endA, startB, endB) 
 	{
 
-}
+};
 
 function findFirstMatch(text, searchString) 
 	{
 
-}
+};
 
 function findLastMatch(text, searchString) 
 	{
 
-}
+};
 
 function substringBetweenMatches(text, searchString) 
 	{
 
-}
+};
