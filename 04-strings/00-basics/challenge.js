cbcb63c89a7f0f3b80cc6adff921f9b3a81f0aca
xafirelove <xafirelove@outlook.com>
2014-08-07 20:38:09 +0000

    modified challenge file

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,17 +1,26 @@
-module.exports.formLetter = function formLetter(firstName, senderName, message) {
+formLetter = function(firstName, senderName, message) {
 	
-	var greeting = "Hello ";
-	var firstName = "Rob,";
-	var greetingStr = greeting + firstName + "\n\n";
 	
-	var message = "Take care.";
-	var messageStr = message + "\n\n";
+	var firstName = "Rob"
 	
-	var closing = "Sincerely,";
-	var senderName = "Joe";
-	var closingStr = closing + "\n" + senderName;
+	var senderName = "Joe"
 	
-	console.log("greetingStr" + "messageStr" + "closingStr")
+	var message = "Take care."
+	
+	console.log("Hello " + firstName + "," + "\n\n" + message + "\n\n" + "Sincerely," + senderName);
+	
+	// var greeting = "Hello ";
+	// var firstName = "Rob,";
+	// var greetingStr = greeting + firstName + "\n\n";
+	
+	// var message = "Take care.";
+	// var messageStr = message + "\n\n";
+	
+	// var closing = "Sincerely,";
+	// var senderName = "Joe";
+	// var closingStr = closing + "\n" + senderName;
+	
+	// console.log("greetingStr" + "messageStr" + "closingStr")
 
 	};
 	
