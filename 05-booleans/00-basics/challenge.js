c4eac1f8b35d4cfbd91476e81864aa362585f4e2
xafirelove <xafirelove@outlook.com>
2014-08-14 23:15:57 +0000

    modified challenge file

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,48 +1,31 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
 	
-	if(stringOne == stringTwo)
+	return (stringOne == stringTwo);
 	
-	return(true);
-	
-	else
-	
-	return(false);
-
 };
 
 module.exports.notEqual = function(one, two) {
 	
-	if 
-		(one == "two")
+	return (one !== two);
 	
-	return(false);
-	
-	else {
-		
-		return(true);
-	}
-
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
 	
 	//inbetween(lower, middle, upper)
 	
-	if(lower < middle && middle < upper)
-	
-	return(true);
-	
-	else
-		
-		return(false);
+	return (lower < middle && middle < upper);
+
 };
 
 module.exports.outsideRanges = function(number) {
 
-
+	 return ((number < 10 || number > 20) && (number <= 42 || number > 75) && (number <= 1 || number >= 6));
 
 };
 
 module.exports.nameAndPrice = function(name, price) {
+
+	 return ((name === 'NYTimes') || (name === 'LATimes')) && (price >= 1);
 
 };
