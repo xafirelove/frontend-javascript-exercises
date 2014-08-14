458d00c3593c7ebbc12e858e3cafbd817d1e4299
xafirelove <xafirelove@outlook.com>
2014-08-14 20:52:32 +0000

    modified challenge file

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,16 +1,45 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
+	
+	if(stringOne == stringTwo)
+	
+	return(true);
+	
+	else
+	
+	return(false);
 
 };
 
 module.exports.notEqual = function(one, two) {
+	
+	if 
+		(one != two)
+	
+	return(true);
+	
+	else {
+		
+		return(false);
+	}
 
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	
+	//inbetween(lower, middle, upper)
+	
+	if(upper >= middle >= lower)
+	
+	return(true);
+	
+	else
+		
+		return(false);
 };
 
 module.exports.outsideRanges = function(number) {
+
+
 
 };
 
